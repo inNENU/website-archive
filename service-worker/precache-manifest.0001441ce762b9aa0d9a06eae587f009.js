e556f4eaa2a3011d6301bf3291fc0ff3c714317a
Mister-Hope <zhangbowang1998@gmail.com>
2020-09-21 14:03:47 +0000

    Deploying to gh-pages from  @ 579cd9726917892995e824315c022e07f2320fbd 🚀

---
+++ service-worker/precache-manifest.0001441ce762b9aa0d9a06eae587f009.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/assets/icon/apple-icon-152.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/assets/icon/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/assets/icon/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/assets/icon/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/assets/icon/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/assets/icon/apple-touch-icon.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/assets/icon/chrome-192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/assets/icon/chrome-512.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/assets/icon/favicon-16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/assets/icon/favicon-32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/assets/icon/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/assets/icon/mstile-150.png"
+  },
+  {
+    "revision": "f910898b9e2b661930617b3120473698",
+    "url": "/assets/icon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "95e39d34106d48bf3df7",
+    "url": "/css/app.575a7a45.css"
+  },
+  {
+    "revision": "4565d01823336237536c",
+    "url": "/css/chunk-09ab8b4e.b68693e2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a85b32c8c8e5f1d3799f662b31751582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95e39d34106d48bf3df7",
+    "url": "/js/app.09c2512d.js"
+  },
+  {
+    "revision": "4565d01823336237536c",
+    "url": "/js/chunk-09ab8b4e.f70a694d.js"
+  },
+  {
+    "revision": "c846b04e5fca7b937f8a",
+    "url": "/js/manifest.b6965cd1.js"
+  },
+  {
+    "revision": "ad082a1f23d7f2fd453f769d6d5db133",
+    "url": "/manifest.json"
+  }
+]);
---
+++ service-worker/precache-manifest.912b3120c299ceacc2a98e3e33f43469.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7507d780e15f62543518",
+    "url": "/css/app.575a7a45.css"
+  },
+  {
+    "revision": "272a307dadccf994a7c7",
+    "url": "/css/chunk-09ab8b4e.b68693e2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b5ceab74f0df476fc27b1385049388e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7507d780e15f62543518",
+    "url": "/js/app-legacy.e90707bc.js"
+  },
+  {
+    "revision": "272a307dadccf994a7c7",
+    "url": "/js/chunk-09ab8b4e-legacy.18f18e87.js"
+  },
+  {
+    "revision": "dac0679f7e4df0bb4b0e",
+    "url": "/js/manifest-legacy.0a15e706.js"
+  },
+  {
+    "revision": "ad082a1f23d7f2fd453f769d6d5db133",
+    "url": "/manifest.json"
+  }
+]);
